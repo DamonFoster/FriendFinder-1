5efe040432a5c4cb4fa1dfef1623155b600a5fb4
Hunter Leachman <leachmanh@MacBook-Air-2.attlocal.net>
2016-11-03 12:53:39 -0500

    routes

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,13 +1,11 @@
-// 3   - Your html-routes.js file should include two routes:
-  //      3.1 - A GET Route to /survey which should display the survey page.
-  //      3.2 - A default USE route that leads to home.html which displays the home page.
 var path = require('path');
 module.exports = function (app) {
 	app.get('/survey', function(req,res) {
 		res.sendFile(path.join(__dirname + '/../public/survey.html'));
 	    });
 
-	app.use(function (req, res) {
+	app.get('/', function(req,res) {
 		res.sendFile(path.join(__dirname + '/../public/home.html'));
-	});
+	    });
+
 };
